d435625ea62132dfe35c221a3e23c22ad4e4a096
Stefania <stebofof@gmail.com>
2023-10-11 11:34:45 +0200

    Deleted a js script

--- src/script.js
+++ src/script.js
@@ -21,30 +21,3 @@ function revealOnScroll() {
 // Calls the function when the page loads and when it scrolls
 window.addEventListener("load", revealOnScroll);
 window.addEventListener("scroll", revealOnScroll);
-
-/*
-LEFT SIDE ANIMATION
-*/
-function showAfterScroll() {
-  const animatedElement = document.querySelector(".animated-element");
-
-  function handleScroll() {
-    const elementoTop = animatedElement.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (elementoTop < windowHeight) {
-      animatedElement.style.opacity = 1;
-      animatedElement.style.transform = "translateX(0)";
-      window.removeEventListener("scroll", handleScroll); // Remove the listener once the element has been shown
-    }
-  }
-
-  // Add a listener for the 'scroll' event
-  window.addEventListener("scroll", handleScroll);
-
-  // Call handleScroll on page load to check if the element is already visible
-  handleScroll();
-}
-
-// Call the function when the page loads
-window.addEventListener("load", showAfterScroll);
